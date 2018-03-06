7289e86de3331f3db60fd3fa2fedaa92cdf516b2
dsnatochy <dennis@poynt.co>
2018-03-05 16:43:13 -0800

    clean-up

--- app/src/main/java/com/example/receiptdecoratorservice/ReceiptDecoratorService.java
+++ app/src/main/java/com/example/receiptdecoratorservice/ReceiptDecoratorService.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -34,25 +35,8 @@ public class ReceiptDecoratorService extends Service {
         public void decorate(PrintedReceipt printedReceipt, String requestId, IPoyntReceiptDecoratorServiceListener
                 listener) throws RemoteException {
             List<PrintedReceiptLine> footerLines = printedReceipt.getFooter();
-
-            if (footerLines == null) {
-                footerLines = new ArrayList<>();
-            }
-
-            EMPTY_LINE.setText("\n");
-
-            PrintedReceiptLine customText = new PrintedReceiptLine();
-            customText.setText("Follow us on Facebook");
-
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(customText);
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(EMPTY_LINE);
-
+            updateReceipt(footerLines);
             printedReceipt.setFooter(footerLines);
-
-
             listener.onReceiptDecorated(requestId, printedReceipt);
         }
 
@@ -67,29 +51,35 @@ public class ReceiptDecoratorService extends Service {
             PrintedReceiptSection footerSection = printedReceiptV2.getFooter();
             List<PrintedReceiptLine> footerLines = footerSection.getLines();
 
-            if (footerLines == null) {
-                footerLines = new ArrayList<>();
-            }
-
-            for (PrintedReceiptLine line : footerLines) {
-                Log.d(TAG, "decorate: " + line.getText());
-            }
-            EMPTY_LINE.setText("\n");
-
-            PrintedReceiptLine customText = new PrintedReceiptLine();
-            customText.setText("Follow us on Facebook");
-
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(customText);
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(EMPTY_LINE);
-            footerLines.add(EMPTY_LINE);
+            footerLines = updateReceipt(footerLines);
 
             footerSection.setLines(footerLines);
             printedReceiptV2.setFooter(footerSection);
             listener.onReceiptDecorated(requestId, printedReceiptV2);
         }
     };
+
+    @NonNull
+    private List<PrintedReceiptLine> updateReceipt(List<PrintedReceiptLine> footerLines) {
+        if (footerLines == null) {
+            footerLines = new ArrayList<>();
+        }
+
+        for (PrintedReceiptLine line : footerLines) {
+            Log.d(TAG, "decorate: " + line.getText());
+        }
+        EMPTY_LINE.setText("\n");
+
+        PrintedReceiptLine customText = new PrintedReceiptLine();
+        customText.setText("Follow us on Facebook");
+
+        footerLines.add(EMPTY_LINE);
+        footerLines.add(customText);
+        footerLines.add(EMPTY_LINE);
+        footerLines.add(EMPTY_LINE);
+        footerLines.add(EMPTY_LINE);
+        return footerLines;
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
